ed1ec4ce3d5c3b5b0aa62d7eb00e0b8f6ea880d8
yangshun1029 <yangshun1029@gmail.com>
2016-01-16 17:47:13 +0800

    重写了实现方法

--- MacGap/AppDelegate.m
+++ MacGap/AppDelegate.m
@@ -43,12 +43,33 @@
 
 -(void)startAria2
 {
-    NSString *path = [[NSBundle mainBundle] pathForResource:@"startaria" ofType:@"sh"];
+    NSFileManager *fileManager = [NSFileManager defaultManager];
+    NSString *supportPath=[NSSearchPathForDirectoriesInDomains(NSApplicationSupportDirectory, NSUserDomainMask, YES) objectAtIndex:0];
+    
+    NSString *path = [NSString stringWithFormat:@"%@/%@/sh/",supportPath,[[NSBundle mainBundle] bundleIdentifier]];
+    [fileManager createDirectoryAtPath:path withIntermediateDirectories:YES attributes:nil error:NULL];
+    NSString *startAriaPath = [path stringByAppendingPathComponent:@"startaria.sh"];
+    BOOL res=[fileManager createFileAtPath:startAriaPath contents:nil attributes:nil];
+    if (res) {
+        NSLog(@"创建启动脚本成功");
+    } else {
+        NSLog(@"创建启动脚本失败");
+    }
+    
+    NSString *shCommand = [NSString stringWithFormat:@"touch \"/Users/Shared/aria2.session\"\n %@ --input-file=\"/Users/Shared/aria2.session\" --save-session=\"/Users/Shared/aria2.session\" --save-session-interval=10 --dir=\"$HOME/Downloads/\" --max-connection-per-server=10 --max-concurrent-downloads=10 --continue=true --split=10 --min-split-size=10M --enable-rpc=true --rpc-listen-all=false --rpc-listen-port=6800 --rpc-allow-origin-all --check-integrity=true --bt-enable-lpd=true --follow-torrent=true --user-agent=\"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/601.3.9 (KHTML, like Gecko) Version/9.0 Safari/601.3.9\" -c -D",[[NSBundle mainBundle] pathForResource:@"aria2c" ofType:nil]];
+    
+    BOOL writeRes =[shCommand writeToFile:startAriaPath atomically:YES encoding:NSUTF8StringEncoding error:nil];
+    if(writeRes) {
+        NSLog(@"写入启动脚本成功");
+    } else {
+        NSLog(@"写入启动脚本失败");
+    }
+    
     NSTask *task = [[NSTask alloc] init];
     task.launchPath = @"/bin/sh";
-    task.arguments = @[path];
+    task.arguments = @[startAriaPath];
     [task launch];
-    //NSArray *arguments = [NSArray arrayWithObjects: @"--enable-rpc=true", @"--check-integrity=true",@"--rpc-listen-all=true", @"--rpc-allow-origin-all", @"--max-connection-per-server=10",@"--max-concurrent-downloads=10",@"--min-split-size=10M",@"--split=10",@"--bt-enable-lpd=true",@"--bt-max-peers=55",@"--bt-require-crypto=true",@"-c",@"-D", @"-d",savePath, nil];
+    
 }
 
 -(void)closeAria2
